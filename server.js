081094b153811fa0bad0b32a8fe989c51d370d9b
14581 <30729991+14581@users.noreply.github.com>
2017-09-19 10:09:22 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ function hash(input,salt) {
 }
 app.get('/hash/:input',function(req,res){
 var hashed = hash(req.params.input,'This-isarandomstring');
-return hashed.toString('hex');
-// res.send(hashed.toString('hex'));
+ res.send(hashed.toString('hex'));
 });
   
   
